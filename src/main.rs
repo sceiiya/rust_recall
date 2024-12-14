1cd425bcf4ceda6608acab69c42ba74ee20572bc
sceiiya <meetscheidj@gmail.com>
2024-12-14 16:43:15 +0800

    update

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,20 @@
+// use std::io;
+// fn main() {
+//     // println!("Hello, world!");
+//     println!("Lets guess!");
+//     println!("input your guess");
+//     let mut guess = String::new();
+
+//     io::stdin()
+//     .read_line(&mut guess).expect("Faile to read line!")
+//     .expect("failed to get num");
+
+//     println!("your guess is {guess}")
+// }
+
+
 fn main() {
-    println!("Hello, world!");
+
 }
+
+
