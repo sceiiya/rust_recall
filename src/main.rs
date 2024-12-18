56d65cbc4cb7df448892d0a02c039765a95599c8
sceiiya <meetscheidj@gmail.com>
2024-12-19 02:31:47 +0800

    push up

--- src/main.rs
+++ src/main.rs
@@ -12,9 +12,28 @@
 //     println!("your guess is {guess}")
 // }
 
+use std::default;
+
 
 fn main() {
+    let logical : bool = true;
+    
+    let a_float : f64 = 1.0;
+    let an_integer = 5i32;
 
+    let default_float = 3.0;
+    let default_integer = 8;
+
+    let mut inferred_type = 13;
+    inferred_type = 32;
+
+    print!("{logical}");
+    println!("{a_float}");
+    println!("{}",an_integer);
+    println!("{}", inferred_type);
+    // println!("{}",inferred_type);
+    println!("{default_float}");
+    println!("{default_integer}");
 }
 
 
